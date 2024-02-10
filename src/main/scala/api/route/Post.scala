4ca1b60f04896e700ff545a184fa69e0b255cda3
unknown <pedro_gonsalves@hotmail.com>
2024-02-09 23:28:30 -0300

    add: exception control and status code return. add json formats to write json body of return

--- src/main/scala/api/route/Post.scala
+++ src/main/scala/api/route/Post.scala
@@ -3,20 +3,23 @@ package api.route
 import api.json.{TransacaoJSF, TransacaoJSM}
 import akka.http.scaladsl.server.{Directives, Route}
 import akka.http.scaladsl.model.StatusCodes
-import database.models.Transacoes
+import org.json4s.native.Serialization
+import database.models.{Cliente, Transacao}
 import database.operations.addTransaction
-
+import database.operations.getClientInfo
+import org.json4s.DefaultFormats
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 class Post extends Directives with TransacaoJSF {
 
+  implicit val formats: DefaultFormats.type = org.json4s.DefaultFormats
 
   val route: Route =
     path("clientes" / LongNumber / "transacoes") { clienteId =>
       post {
         entity(as[TransacaoJSM]) { trns =>
-          val novaTrns = Transacoes(
+          val novaTrns = Transacao(
             id=Some(1),
             id_cliente=clienteId,
             valor=trns.valor,
@@ -26,8 +29,25 @@ class Post extends Directives with TransacaoJSF {
           )
           val novaTrnsOp: Future[Unit] = addTransaction(novaTrns)
           onComplete(novaTrnsOp) {
-            case Success(_) => complete(StatusCodes.Created)
-            case Failure(exception) => complete(StatusCodes.InternalServerError, exception.getMessage)
+
+            case Success(_) =>
+              val clientInfo: Future[Option[Cliente]] = getClientInfo(clienteId)
+              onComplete(clientInfo) {
+
+                case Success(Some(cliente)) =>
+                  complete(StatusCodes.OK, Serialization.write(Map("saldo" -> cliente.saldo, "limite" -> cliente.limite)))
+                case Failure(exception) =>
+                  complete(StatusCodes.InternalServerError, s"Message:${exception.getMessage}")
+              }
+
+            case Failure(exception) =>
+              if (exception.getMessage.contains("violates foreign key")) {
+                complete(StatusCodes.NotFound)
+              } else if (exception.getMessage.contains("Limite excedido")) {
+                complete(StatusCodes.UnprocessableContent)
+              } else {
+                complete(StatusCodes.InternalServerError, s"Message:${exception.getMessage}")
+              }
           }
 
         }
@@ -35,4 +55,3 @@ class Post extends Directives with TransacaoJSF {
 
     }
 }
-
